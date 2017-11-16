fe142b3cef2aba2506f069c9fc14ed715b22b3ee
renaiku <corbeau.lenaic@gmail.com>
2017-11-16 21:22:54 +0100

    LOGS in console

--- webadmin.lua
+++ webadmin.lua
@@ -2,6 +2,21 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -- Rcon commands
+RegisterCommand("weblog", function(source, args, rawCommand)
+	local argz = string.gsub(rawCommand, "weblog ", "")
+	argz = json.decode(argz)
+	print("")
+	print("====================[ESX - WEB ADMIN]====================")
+	print(argz['user'] .. " used '" .. argz['action'] .. "' with the following parameters:")
+	for k,v in pairs(argz) do
+		if k ~= 'user' and k ~= 'action' then
+			print("    " .. k .. ": " .. v)
+		end
+	end
+	print("=========================================================")
+	print("")
+end, true)
+
 RegisterCommand("setjob", function(source, args, rawCommand)
 	local xPlayer = ESX.GetPlayerFromIdentifier(args[1])
 	xPlayer.setJob(args[2], tonumber(args[3]))
